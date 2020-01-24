c96b54c1fcb1a3d4c85a0c6bdb5cf63800719fd3
kelvin <kelvin@serel.com.br>
2020-01-24 10:18:07 -0300

    Jogada especial promoção

--- Xadrez/Xadrez/PartidaDeXadrez.cs
+++ Xadrez/Xadrez/PartidaDeXadrez.cs
@@ -143,6 +143,21 @@ namespace Xadrez
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = tab.peca(destino);
+
+            //#jogadaespecial promoçã
+            if(p is Peao)
+            {
+                if(p.cor == Cor.Branca && destino.Linha == 0 || p.cor == Cor.Preta && destino.Linha == 7)
+                {
+                    p = tab.RetirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab,p.cor);
+                    tab.ColocarPeca(dama, destino);
+                    pecas.Add(dama); 
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -160,9 +175,7 @@ namespace Xadrez
             {
                 turno++;
                 mudaJogador();
-            }
-
-            Peca p = tab.peca(destino);
+            }            
 
             // #jogadaespecial en passant
             if(p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
